79116bbd712e1cf594973bd1f3a833c1dc719d1c
robocup <robocup@TURTLE-5K-1.(none)>
2015-11-13 13:15:59 +0100

    comments are added. Also parameters and safety's

--- src/uart.cpp
+++ src/uart.cpp
@@ -1,19 +1,28 @@
+//This code wait on a Float32MultiArray message. This values will be send to serial ports.
+//the values needs to be given in mps. a converter factor can be given in a paramter. Also the max and min speed are parameters.
+//
+//author 	: Niek Francke
+//date 		: 13-11-2015
+
 #include <SerialStream.h>
 #include <iostream>
 #include <unistd.h>
 #include <cstdlib>
 #include <string>
-#include <geometry_msgs/Twist.h>
 #include <std_msgs/Float32MultiArray.h>
-
 #include "ros/ros.h"
+
+#define MOTORDRIVER_START_OF_FRAME 	0x5a
+#define MOTORDRIVER_TYPE 			0xaa
+#define MOTORDRIVER_CMD				0x03
+#define MOTORDRIVER_END_OF_FRAME	0x00
+#define TOPIC_NAME 					"mcWheelVelocityMps"
+#define TOPIC_BUFFER_SIZE			1000
 
 using namespace std;
 using namespace LibSerial;
 
-//
-// Init the serial port.
-//
+//Define serial ports
 SerialStream serial_port5;
 SerialStream serial_port7;
 SerialStream serial_port8;
@@ -21,62 +30,111 @@ SerialStream serial_port8;
 class Subscribe
 {
 public:
-	struct Output{ char cOutBuf[8]; int iSpeed;};
-	Output serialPorts[10];	
+	struct Output{ char cOutBuf[8]; int iSpeed;};	//data for serialports
+	Output serialPorts[10];							
+	int iConvertFactor;								//this factor will convert data from RPM to pulses/time value
+	int iMaxPulseSpeed; 
+	int iMinPulseSpeed;
 
+	//Function: create class and read params
+	//pre: 	-
+	//post: iConvertFactor will be 0 if there is no param readed.
 	Subscribe(ros::NodeHandle nh)
 	{
-		sub = nh.subscribe("mcWheelVelocityMps",1000, &Subscribe::commandRpmReceived, this);
+		sub = nh.subscribe(TOPIC_NAME,TOPIC_BUFFER_SIZE, &Subscribe::commandRpmReceived, this);
 		
+		//Reading param. This param will be used to convert the data from RPM to pulses.
+		std::string sParamName = "iConvertFactor";
+		//check if param is defined.
+		if (nh.hasParam(sParamName)){
+			nh.getParam(sParamName, iConvertFactor);
+			ROS_INFO("parameter iConvertFacotr is readed and has value :%i", iConvertFactor);
+		}else{
+			ROS_ERROR("Parameter iConvertFactor does not exist");
+			iConvertFactor = 0;
+		}
 
-//		nh.setParam("iConvertFactor",100);
-//		const std::string sParamName = "iConvertFactor";
-//		int iTest = 20;
-//		if(!ros::param::get(sParamName,iTest)){
-//			ROS_ERROR("Could not get parameter");
-//		}
+		//Reading param. This param will be used to convert the data from RPM to pulses.
+		sParamName = "iMaxPulseSpeed";
+		//check if param is defined.
+		if (nh.hasParam(sParamName)){
+			nh.getParam(sParamName, iMaxPulseSpeed);
+			ROS_INFO("parameter iMaxPulseSpeed is readed and has value :%i", iMaxPulseSpeed);
+		}else{
+			ROS_ERROR("Parameter iMaxPulseSpeed does not exist");
+			iMaxPulseSpeed = 0;
+		}
+
+		//Reading param. This param will be used to convert the data from RPM to pulses.
+		sParamName = "iMinPulseSpeed";
+		//check if param is defined.
+		if (nh.hasParam(sParamName)){
+			nh.getParam(sParamName, iMinPulseSpeed);
+			ROS_INFO("parameter iMinPulseSpeed is readed and has value :%i", iMinPulseSpeed);
+		}else{
+			ROS_ERROR("Parameter iMinPulseSpeed does not exist");
+			iMinPulseSpeed = 0;
+		}
 	}
 
+	//function: this function responds on a float32MultiArray message.
+	//pre: values for array places 4,6,7, because the serial ports are 5,7 and 8
+	//post: -
 	void commandRpmReceived(const std_msgs::Float32MultiArray::ConstPtr& msg)
 	{
-		ROS_INFO("Float32MultiArray received");
+		ROS_INFO_ONCE("first time a FLoat32MultiArray is received");
+		ROS_DEBUG("Float32MultiArray received");
 
+		//prints the data from FLoat32MultiArray to log
 		for(int i = 0; i < 10 ; i++){
-			ROS_INFO("data poort %i = %f" , i, msg->data[i]);
+			ROS_DEBUG("data poort %i = %f" , i, msg->data[i]);
 		}
 
+		//writes the data from Float32MultiArray to iSpeed for serialport.
 		for(int i = 0 ; i < 10 ; i++){
-			serialPorts[i].iSpeed = msg->data[i];
+			serialPorts[i].iSpeed = (int)msg->data[i];
+
+			//overload protection
+			if(serialPorts[i].iSpeed >= iMaxPulseSpeed){
+				serialPorts[i].iSpeed = iMaxPulseSpeed;
+			}
+			if(serialPorts[i].iSpeed <= iMinPulseSpeed){
+				serialPorts[i].iSpeed = iMinPulseSpeed;
+			}
 		}	
-
-		//for testing
-//		serialPorts[5].iSpeed = msg->data[0];
-//		serialPorts[7].iSpeed = msg->data[0];
-//		serialPorts[8].iSpeed = msg->data[0];
-				
+		
+		//Make the messages for RS422 (motordrivers)	
 		for(int i = 0 ; i < 10 ; i++){
-			serialPorts[i].cOutBuf[0] = 0x5a; //start of frame
-			serialPorts[i].cOutBuf[1] = 0xaa; //start of frame
-			serialPorts[i].cOutBuf[2] = 0x03; //start of frame
-			serialPorts[i].cOutBuf[3] = (serialPorts[i].iSpeed & 0xff);			//speed
-			serialPorts[i].cOutBuf[4] = (serialPorts[i].iSpeed >> 8) & 0xff;	//speed
-			serialPorts[i].cOutBuf[5] = 0x00; //??
-			serialPorts[i].cOutBuf[6] = 0x00; //??
-			serialPorts[i].cOutBuf[7] = 0x00; //eof
+			serialPorts[i].cOutBuf[0] = MOTORDRIVER_START_OF_FRAME;
+			serialPorts[i].cOutBuf[1] = MOTORDRIVER_TYPE; 
+			serialPorts[i].cOutBuf[2] = MOTORDRIVER_CMD; 
+			serialPorts[i].cOutBuf[3] = (serialPorts[i].iSpeed & 0xff);
+			serialPorts[i].cOutBuf[4] = (serialPorts[i].iSpeed >> 8) & 0xff;
+			serialPorts[i].cOutBuf[5] = 0x00; //data 2 (not defined)
+			serialPorts[i].cOutBuf[6] = 0x00; //data 2 (not defined)
+			serialPorts[i].cOutBuf[7] = MOTORDRIVER_END_OF_FRAME;
 		}
 
-		ROS_INFO("data poort 5 = %i" , serialPorts[5-1].cOutBuf[3]);
-		ROS_INFO("data poort 5 = %i" , serialPorts[5-1].cOutBuf[4]);	
-			
+		//print speed data that will be send to motordrivers.
+		for(int i = 0 ; i < 10 ; i++){
+			ROS_DEBUG("data poort %i byte 3 = %i" , i,serialPorts[i].cOutBuf[3]);
+			ROS_DEBUG("data poort %i byte 4 = %i" , i,serialPorts[i].cOutBuf[4]);	
+		}
+
+		//Write serial ports. the value will be minus 1 because it will match with the serialPorts array.	
       	serial_port5.write(serialPorts[5-1].cOutBuf, 8);
       	serial_port7.write(serialPorts[7-1].cOutBuf, 8);
 	   	serial_port8.write(serialPorts[8-1].cOutBuf, 8);
 	}
 
 private:
-	ros::Subscriber sub;
+	ros::Subscriber sub;	//define ros subscriber
 };//end of class Subscribe
 
+//function: initialize serial port "string sSerialPort" as follows:
+//			baud = 115200, char_size = 8, parity = none, stop bits = 2 and no flauw control
+//pre: 
+//post: return 1 if its done. return 0 if it fails.		
 bool initSerialPort(SerialStream& serial_port, string sSerialPort);
 
 
@@ -85,9 +143,10 @@ int main(int argc, char **argv  )
 	//initialize ROS
 	ros::init(argc, argv, "mcMotorDriver");
 	
+	//create nodehandle
 	ros::NodeHandle nh;
 
-	//check if init serial went good.
+	//check if init serial went good. otherwise close program.
 	if((initSerialPort(serial_port5,"/dev/ttyS5") 
 		&& initSerialPort(serial_port7,"/dev/ttyS7") 
 		&& initSerialPort(serial_port8,"/dev/ttyS8")) 
@@ -99,13 +158,16 @@ int main(int argc, char **argv  )
 	//create class
 	Subscribe Sobject(nh);
 
-	//	ros::Subscriber sub = nh.subscribe("mcWheelVelocityMps",1000, commandRpmReceived);	
-
+	//wait until a Float32MulitArray is received and run the callback function
 	ros::spin();
 
 	return 0;
 }
 
+//function: initialize serial port "string sSerialPort" as follows:
+//			baud = 115200, char_size = 8, parity = none, stop bits = 2 and no flauw control
+//pre: 
+//post: return 1 if its done. return 0 if it fails.		
 bool initSerialPort(SerialStream& serialPort,string sSerialPort){
 	//
 	//Open the serial port
@@ -131,7 +193,7 @@ bool initSerialPort(SerialStream& serialPort,string sSerialPort){
 		ROS_ERROR("Error: Could not set the baud rate.");
 		return 0;
 	}
-	ROS_INFO("baud rate is setted");
+	ROS_INFO("baud rate is set");
 
 	//
 	// Set the number of data bits.
@@ -142,7 +204,7 @@ bool initSerialPort(SerialStream& serialPort,string sSerialPort){
 		ROS_ERROR("Error: Could not set the character size.");
 		return 0;
 	}
-	ROS_INFO("char size is setted");
+	ROS_INFO("char size is set");
 
 	//
 	// Disable parity.
@@ -164,7 +226,7 @@ bool initSerialPort(SerialStream& serialPort,string sSerialPort){
 		ROS_ERROR("Error: Could not set the number of stop bits.");
 		return 0;
 	}
-	ROS_INFO("stop bits are setted");
+	ROS_INFO("stop bits are set");
 
 	//
 	// Turn off hardware flow control.
